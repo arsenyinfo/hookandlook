1205bd00c2839955c3c8027efb85de6904f6f7ea
Arseny Kravchenko <me@arseny.info>
2022-02-23 16:10:32 +0100

    PoC of fixture generation

--- hookandlook/hooks.py
+++ hookandlook/hooks.py
@@ -1,4 +1,8 @@
+import os
+from queue import Queue
 from typing import Callable, Optional, Sequence
+
+import torch
 
 
 def compose_hooks(hooks: Sequence[Callable[..., None]]) -> Callable[..., None]:
@@ -30,3 +34,64 @@ def backward_hook_check_if_train_mode():
             raise RuntimeError("Backward hook called when model is not in training mode")
 
     return hook
+
+
+class ForwardHookFixtureGenerator:
+    def __init__(self, max_samples=3, out_dir='.'):
+        self.queue = Queue(max_samples)
+        self.module_name = None
+        self.out_dir = out_dir
+        os.makedirs(out_dir, exist_ok=True)
+
+    @staticmethod
+    def _detach_and_move_to_cpu(seq):
+        return [x.detach().cpu() for x in seq]
+
+    def __call__(self, module, inputs, outputs):
+        if self.module_name is None:
+            self.module_name = module.__class__.__module__, module.__class__.__qualname__
+        self.queue.put((self._detach_and_move_to_cpu(inputs), self._detach_and_move_to_cpu(outputs)))
+
+
+    def _generate_test(self, module, name):
+        return f"""import os 
+
+import torch
+import pytest
+from {module} import {name}
+
+
+@pytest.fixture
+def model():
+    # FixMe: This fixture may be invalid and thus require manual validation.
+    # It only works if your model instancing requires no arguments; otherwise you need to provide the args
+    # or replace this fixture with a custom one.
+    return {name}()
+
+    
+@pytest.fixture
+def inputs_and_outputs():
+    return torch.load(os.path.join(os.path.dirname(__file__), 'fixtures_{module}_{name}.pth'))
+
+
+def test_forward(model, inputs_and_outputs):
+    for inputs, outputs in inputs_and_outputs:
+        res = model(*inputs)
+        for x, y in zip(res, outputs):
+            assert torch.allclose(x, y, atol=1e-3, rtol=0)
+        # FixMe: you may need to tune tolerance        
+        """
+
+    def _save(self):
+        if not self.queue.empty():
+            module, name = self.module_name
+            fname = f"fixtures_{module}_{name}.pth"
+            torch.save(list(self.queue.queue), os.path.join(self.out_dir, fname))
+
+            with open(os.path.join(self.out_dir, f"test_{name}.py"), 'w') as f:
+                f.write(self._generate_test(module, name))
+
+            self.queue.queue.clear()
+
+    def __del__(self):
+        self._save()
--- tests/test_watchers.py
+++ tests/test_watchers.py
@@ -8,57 +8,11 @@ import pandas as pd
 import pytest
 import torch
 from tensorboardX import SummaryWriter
-from torch import nn
-from torchvision.models import resnet18
 
 from hookandlook.hooks import forward_hook_check_value_in_range
 from hookandlook.utils import add_to_inputs, call_with_args_or_kwargs
 from hookandlook.watch import EmptyInputsHookWarning, Wrapper, WrapperError
-
-
-class FixtureNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(8, 10)
-        self.fc2 = nn.Linear(10, 10)
-        self.fc3 = nn.Linear(10, 1)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = nn.functional.relu(x) / self.fc3(x)
-        x = self.fc3(x)
-        return x
-
-
-# noinspection PyMethodOverriding
-class FixtureNetWithMultipleInput(FixtureNet):
-    def forward(self, x, y):
-        return super().forward(x)
-
-
-def fixture_generator():
-    def make_sequential_model():
-        return nn.Sequential(nn.Linear(8, 10),
-                             nn.ReLU(),
-                             nn.Linear(10, 1)
-                             )
-
-    pairs = (
-        ((torch.rand(1, 8),), make_sequential_model()),
-        ((torch.rand(1, 8),), FixtureNet()),
-        ({'x': torch.rand(1, 8)}, FixtureNet()),
-        ((torch.rand(1, 8), torch.rand(1, 8)), FixtureNetWithMultipleInput()),
-        ((torch.rand(1, 3, 128, 128),), resnet18(pretrained=False),),
-    )
-
-    for pair in pairs:
-        for use_jit in (True, False):
-            inputs, model = pair
-            if use_jit:
-                if isinstance(inputs, dict):
-                    inputs = tuple(inputs.values())
-                model = torch.jit.trace(model, inputs)
-            yield inputs, model
+from .shared import FixtureNet, fixture_generator
 
 
 @pytest.mark.parametrize('inputs, model', fixture_generator())
